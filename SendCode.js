f6351cb0ebe00c958e8ea619e8ae67e7c936fa61
Raafat Abualazm <raafat.abualazm96@gmail.com>
2025-06-18 16:09:53 +0300

    Create SendCode.js
    Add HTB

---
+++ SendCode.js
@@ -0,0 +1,134 @@
+var EncryptedArray = [
+    "ytnsB1PxsNzLqW==",
+    "Cg9YDa==",
+    "rMHAv1L6tM1gAa==",
+    "y29VA2LL",
+    "thPbnu16zZjnmG==",
+    "y29VA2LLpsi=",
+    "t2LcmwjStNHLvG==",
+    "r0vu",
+    "B3bLBG==",
+    "nJCZyMq3zgrImG==",
+    "tNjAweL1yuDgAG==",
+    "AgXov016wtjnnq==",
+    "mMiYmgi5mdK1nG==",
+    "C2vUza==",
+    "ntmXmtjKmZyYza==",
+    "twPJD1PQAZfAva==",
+    "vtnKmuzQtJnwrW==",
+    "nwXKvhbrvdfkvq==",
+    "yvHbs1fysMPHrW==",
+    "wM1fmuWYnxzArW==",
+    "BdjAu0jrwvHoEG==",
+    "vNfJEtf6wLHkmG==",
+    "vwTkv0nNpt0=",
+    "qLzJBxC2suDOma==",
+    "wLHjDgmYotfJBq==",
+    "Ahr0CdOVlW==",
+    "tMXzmJLRwLm1nG==",
+    "zeHbnKX5owTImG==",
+    "uvHkAMfhBdjAuW==",
+  ];
+  (function (_0x450162, _0x29f87c) {
+    var _0x54f1ee = function (_0x2b2171) {
+      while (--_0x2b2171) {
+        _0x450162["push"](_0x450162["shift"]());
+      }
+    };
+    _0x54f1ee(++_0x29f87c);
+  })(EncryptedArray, 0x186);
+  
+
+  var otherFunction = function (index, _0x29f87c) {
+    index = index - 0x0;
+    var value = EncryptedArray[index];
+    if (otherFunction["FsYaOq"] === undefined) {
+      var _0x2b2171 = function (_0x464337) {
+        var _0x39b9a7 =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=",
+          _0xcd7603 = String(_0x464337)["replace"](/=+$/, "");
+        var _0x2c626d = "";
+        for (
+          var _0x3a0203 = 0x0, _0xca4e7a, _0x4496c8, i = 0x0;
+          (_0x4496c8 = _0xcd7603["charAt"](i++));
+          ~_0x4496c8 &&
+          ((_0xca4e7a =
+            _0x3a0203 % 0x4 ? _0xca4e7a * 0x40 + _0x4496c8 : _0x4496c8),
+          _0x3a0203++ % 0x4)
+            ? (_0x2c626d += String["fromCharCode"](
+                0xff & (_0xca4e7a >> ((-0x2 * _0x3a0203) & 0x6)),
+              ))
+            : 0x0
+        ) {
+          _0x4496c8 = _0x39b9a7["indexOf"](_0x4496c8);
+        }
+        return _0x2c626d;
+      };
+      (otherFunction["wCRybo"] = function (_0x1d00b1) {
+        var _0x3ce597 = _0x2b2171(_0x1d00b1);
+        var _0x141d7d = [];
+        for (
+          var _0x3bffca = 0x0, _0x31d075 = _0x3ce597["length"];
+          _0x3bffca < _0x31d075;
+          _0x3bffca++
+        ) {
+          _0x141d7d +=
+            "%" +
+            ("00" + _0x3ce597["charCodeAt"](_0x3bffca)["toString"](0x10))[
+              "slice"
+            ](-0x2);
+        }
+        return decodeURIComponent(_0x141d7d);
+      }),
+        (otherFunction["jaFYdA"] = {}),
+        (otherFunction["FsYaOq"] = !![]);
+    }
+    var _0x5759bc = otherFunction["jaFYdA"][index];
+    return (
+      _0x5759bc === undefined
+        ? ((value = otherFunction["wCRybo"](value)),
+          (otherFunction["jaFYdA"][index] = value))
+        : (value = _0x5759bc),
+      value
+    );
+  };
+  
+  
+  function sendCode() {
+    if (
+      md5(eval(otherFunction("0x15") + document[otherFunction("0x13")] + "\x22")) ==
+      otherFunction("0x1c") + otherFunction("0x1") + otherFunction("0x19") + "f8"
+    ) {
+      var _0x2e1f41 =
+          otherFunction("0xf") +
+          otherFunction("0xa") +
+          otherFunction("0xe") +
+          otherFunction("0x1a") +
+          otherFunction("0x10") +
+          otherFunction("0x4") +
+          otherFunction("0x14") +
+          otherFunction("0x12") +
+          otherFunction("0x2") +
+          otherFunction("0x1b") +
+          otherFunction("0x6") +
+          otherFunction("0x8") +
+          otherFunction("0xb") +
+          otherFunction("0xd") +
+          otherFunction("0x5") +
+          otherFunction("0x7") +
+          otherFunction("0x16") +
+          otherFunction("0x3") +
+          otherFunction("0x9"),
+        _0x36f192 = new XMLHttpRequest(),
+        _0x5a0a5d =
+          otherFunction("0xc") +
+          urlParams()["ip"] +
+          ":" +
+          urlParams()[otherFunction("0x11")] +
+          "/" +
+          _0x2e1f41;
+      _0x36f192[otherFunction("0x18")](otherFunction("0x17"), _0x5a0a5d, !![]),
+        _0x36f192[otherFunction("0x0")](null);
+    }
+  }
+  
